873121bd3ca34f085a4fc07d624ba5cd5287baaa
Patrick Szmucer <patrick@MacBook-Pro.local>
2014-12-30 00:01:33 +0100

    Initial Commit

---
+++ Oscillator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Oscillator
+//
+//  Created by Patrick Szmucer on 30/12/2014.
+//  Copyright (c) 2014 Patrick Szmucer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
